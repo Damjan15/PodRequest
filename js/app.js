7fed3506a290962ae0e29adb076fb45c0e891645
Damjan15 <stamenovdamjan20@gmail.com>
2022-11-15 22:38:26 +0100

    script added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+/*=============== SELECTORS ===============*/
+const errorDesktop = document.getElementById("errorDesktop");
+const emailDesktop = document.getElementById("emailDesktop");
+const errorMobile = document.getElementById("errorMobile");
+const emailMobile = document.getElementById("emailMobile");
+const form = document.getElementById("emailForm");
+
+/*=============== FUNCTIONS ===============*/
+function checkEmail(e) {
+    e.preventDefault();
+    
+    // Show error on desktop
+    if (!emailDesktop.value) {
+        errorDesktop.classList.remove("hidden");
+        errorDesktop.classList.add("block");
+    }
+
+    // Show error on mobile
+    if (!emailMobile.value) {
+        errorMobile.classList.remove("hidden");
+        errorMobile.classList.add("block");
+    }
+}
+
+/*=============== EVENT LISTENERS ===============*/
+form.addEventListener("submit", checkEmail);
